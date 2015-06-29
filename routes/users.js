6374f0996eb8d589cd4860d3ecd0742e91eee5a3
jc.esquivelojeda <jc.esquivelojeda@gmail.com>
2015-06-28 22:53:41 -0500

    anexo users.js users.ejs

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+	res.render('users');
+ // res.send('ruta users');
 });
 
 module.exports = router;
